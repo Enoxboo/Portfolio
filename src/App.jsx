1a388e143297863df28a99b62f1c4a74a3b06ec5
Enoxboo <matteo.marquant@ynov.com>
2025-11-18 14:46:08 +0100

    fix(section): rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import Layout from './components/layout/Layout';
+import Hero from './components/sections/Hero';
+import About from './components/sections/About';
+import FeaturedProject from './components/sections/FeaturedProject';
+import Projects from './components/sections/Projects';
+import Contact from './components/sections/Contact';
+
 function App() {
     return (
-        <h1 className="text-3xl font-bold underline text-blue-500">
-            Hello Tailwind v4!
-        </h1>
-    )
+        <Layout>
+            <Hero />
+            <About />
+            <FeaturedProject />
+            <Projects />
+            <Contact />
+        </Layout>
+    );
 }
 
-export default App
+export default App;
